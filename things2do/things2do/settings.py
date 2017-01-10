b810f904fac412c4376f73e6dd1a1fe5c0e86684
Abcdefghijklmnopqrstuvwxyzxyz <huang_yue_zhi@outlook.com>
2017-01-10 08:12:35 -0600

    Delete Secrect Key Value
    I deleted the Secrect Key value to make sure someone will not use it to attack the local database.

--- things2do/things2do/settings.py
+++ things2do/things2do/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j_k6kyvv((3*wg(9cqtz-az_*q&n0^*ebhmei%!=hn7geq2$q_'
+SECRET_KEY = 'YOUR_KEY_HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
